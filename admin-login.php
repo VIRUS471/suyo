0d42f91b655d26dcb7556f144b1c244a05566fb9
VIRUS471 <45210574+VIRUS471@users.noreply.github.com>
2018-11-21 02:07:01 +0800

    Create admin-login.php

---
+++ admin-login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Admin Login</title>
+  </head>
+  <body>
+    <h1>Hello header</h1>
+
+    <p>Hello paragraph</p>
+
+
+  </body>
+</html>
