2ae6f57e186ee062be3baa7c4d88c124ba31f8dc
VIRUS471 <45210574+VIRUS471@users.noreply.github.com>
2018-12-31 00:57:09 +0800

    this version has problems with connection:
    Fatal error: Uncaught Error: Call to undefined function mysql_error() in 
    C:\xampp\htdocs\suyo\admin-login.php:30 Stack trace: #0 {main} thrown in 
    C:\xampp\htdocs\suyo\admin-login.php on line 30

--- admin-login.php
+++ admin-login.php
@@ -9,8 +9,42 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
   <title>Admin Login</title>
+
+
+
 </head>
 <body>
+  <?php
+  require 'conn.php';
+  session_start();
+
+if(isset($_POST["txtuser"])){
+$adminid = stripslashes($_REQUEST['txtuser']); // removes backslashes
+$adminid = mysqli_real_escape_string($mysqli,$adminid); //escapes special characters in a string
+$password = stripslashes($_REQUEST['txtpassword']);
+$password = mysqli_real_escape_string($mysqli,$password);
+
+$query = "SELECT * from 'users' where email='$adminid' and passord='$password' ";
+try {
+
+$result = mysqli_query($mysqli,$query) or die(mysql_error());
+$rows = mysqli_num_rows($result);
+    if($rows==1){
+  $_SESSION['id'] = $adminid;
+        }
+        else{
+          echo "this not exist";
+        }
+      }
+        catch (\Exception $e) {
+          echo $e;
+        }
+
+}else {
+
+
+   ?>
+
   <div class="card">
     <div class="card-header">Login</div>
     <div class="card-body">
@@ -19,7 +53,7 @@
         <br/>
         <input type="text" name="txtpassword" id="txtpassword" placeholder="password">
         <br/>
-        <button type="button" class="btn btn-primary mb-2" name="btnsubmit" onclick="check()">Submit</button>
+        <button type="submit" class="btn btn-primary mb-2" name="btnsubmit" onclick="check()">Submit</button>
         <span id="message"></span>
       </form>
     </div>
@@ -30,15 +64,8 @@
     var name = $('#txtuser').val()
     var password= $('#txtpassword').val()
     if (name == ' ' || password == ' '){
-      $.ajax({
-        success:function(){
-          $("form").trigger("reset");
-          $('#message').fadeIn().html("missing Fields");
-          setTimeout(function(){
-            $('#message').fadeOut("Slow");
-          }, 2000);
-        }
-      });
+      $('#message').html(' ');
+      $('#message').html("Required fields");
     }
     else
     {
@@ -56,7 +83,7 @@
 
   }
   </script>
-
+<?php } ?>
 </body>
 </html>
 
---
+++ conn.php
@@ -0,0 +1,13 @@
+<?php
+$db_username = 'root';
+$db_password = '';
+$db_name = 'suyoofficialfinale';
+$db_host = 'localhost:3307';
+
+//connect to MySql
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+if ($mysqli->connect_error) {
+    die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+}
+
+?>
